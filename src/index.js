377b9df6046ef8b000bff5433ab7f1ffdf3cd2dd
Max Pinheiro <maxpinheiro181@gmail.com>
2020-11-26 23:47:47 -0500

    page setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/theme.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <div className="container-fluid min-vw-100 vh-100 bg-sea-green">
+          <div className="vh-100 flex-column row">
+              <div className="col-4"/>
+              <div className="col-8 w-75 row mx-auto">
+                  <div className="col-8">
+                      <p className="text-right px-3 py-2 display-4 text-white font-weight-normal border-right border-3 border-white">Max Pinheiro <br/> Developer, Designer</p>
+                  </div>
+                  <div className="col-4">
+                      <p className="text-left my-4 pt-2 h3 font-weight-light text-white">Resume</p>
+                      <p className="text-left mt-3  h3 font-weight-light text-white">Project Archive</p>
+                  </div>
+              </div>
+          </div>
+      </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
