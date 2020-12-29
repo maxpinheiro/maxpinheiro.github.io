b5f9906c153dc85231820b5b46fa53bf42928daa
Max Pinheiro <maxpinheiro181@gmail.com>
2020-12-29 15:37:05 -0500

    all that jazz

---
+++ media/projects.js
@@ -0,0 +1,52 @@
+const descriptions = [{
+    "title": "a-eye",
+    "description": "interactive computer vision modules developed for neu's 'your eye and ai' course"
+}, 
+{
+    "title": "spotify visualizer",
+    "description": "web app that creates audio-responsive visualizations linked to spotify user's current playback"
+}, 
+{
+    "title": "figure skating score calculator",
+    "description": "web app for easily calculating figure skating profram scores with IJS scoring system"
+}, 
+{
+    "title": "spotify song recommendation",
+    "description": "a neural embedding system for playlist generation. this was a final project for my Artifical Intelligence course. you can check out the final report here."
+}, 
+{
+    "title": "whiteboard",
+    "description": "whiteboard is a fake LMS (Learning Management System). I built the app throughout the Fall 2020 semester in my Web Development course."
+}, 
+{
+    "title": "maze",
+    "description": ""
+}, 
+{
+    "title": "asteroids",
+    "description": ""
+}, 
+{
+    "title": "pacman",
+    "description": ""
+}, 
+{
+    "title": "minesweeper",
+    "description": ""
+}, 
+{
+    "title": "groupeez",
+    "description": ""
+},
+{
+    "title": "java gui animator",
+    "description": ""
+},
+{
+    "title": "centipede",
+    "description": ""
+},
+{
+    "title": "snake",
+    "description": ""
+}];
--- script.js
+++ script.js
@@ -1,48 +1,107 @@
 const cnv = document.getElementById('background');
 const ctx = cnv.getContext('2d');
-let pos = {x: 0, y: 0};
-let vel = {x: 1.5, y: 1};
+let selectedProject = '';
+
+
+window.onload = () => {
+    $('#selected-project').hide();
+    $('#project-list').html(descriptions.map((project, idx) => `<div class="col-12 col-sm-6 col-md-3"><div class="card card-md my-3" style="cursor: pointer;" id="card${idx+1}"><p class="my-auto card-title h4">${project.title}</p></div></div>`));
+    setCards();
+    window.requestAnimationFrame(update);
+};
 
 function update() {    
-ctx.fillStyle = '#1D8A53';
-ctx.fillRect(0, 0, cnv.width, cnv.height);
+    ctx.fillStyle = '#45AF69';
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-ctx.fillStyle = 'black';
-ctx.beginPath();
-ctx.ellipse(pos.x, pos.y, 25, 25, 0, 0, 2 * Math.PI);
-ctx.fill();
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    //ctx.ellipse(pos.x, pos.y, 25, 25, 0, 0, 2 * Math.PI);
+    //ctx.fill();
 
-pos = {x: pos.x + 1 * vel.x, y: pos.y + 1 * vel.y};
-vel = {x: vel.x * (pos.x >= cnv.width || pos.x <= 0 ? -1 : 1), y: vel.y * (pos.y >= cnv.height || pos.y <= 0 ? -1 : 1)};
+    pos = {x: pos.x + 1 * vel.x, y: pos.y + 1 * vel.y};
+    vel = {x: vel.x * (pos.x >= cnv.width || pos.x <= 0 ? -1 : 1), y: vel.y * (pos.y >= cnv.height || pos.y <= 0 ? -1 : 1)};
 
-window.requestAnimationFrame(update);
+    window.requestAnimationFrame(update);
 }
-
-const map = (x, start1, end1, start2, end2) => (((x - start1) / (end1 - start1) * (end2 - start2)) + start2);
 
 const scrollUp = () => window.scrollTo({top: 0, behavior: 'smooth'});
 
 document.addEventListener('scroll', (e) => {
-const screens = window.scrollY / window.innerHeight;
-const numScreens = Math.floor(screens);
-const nav = $('#navbar');
-//nav.removeClass(['bg-steel-blue', 'bg-dark-gray', 'bg-sea-green', 'bg-dark-violet']);
+    const screens = window.scrollY / window.innerHeight;
+    const numScreens = Math.floor(screens);
+    const nav = $('#navbar');
 
-switch(numScreens) {
-    case 0: //rgb(78, 105, 150) -> rgb(68, 175, 105)
-        nav.css('background-color', `rgb(${map(screens, 0, 1, 78, 68)}, ${map(screens, 0, 1, 105, 175)}, ${map(screens, 0, 1, 150, 105)}) `);
-        break;
-    case 1: //rgb(68, 175, 105) -> rgb(52, 58, 64)
-        nav.css('background-color', `rgb(${map(screens, 1, 2, 68, 52)}, ${map(screens, 1, 2, 175, 58)}, ${map(screens, 1, 2, 105, 64)})`);
-        break;
-    case 2: //rgb(52, 58, 64) -> rgb(127, 98, 137)
-        nav.css('background-color', `rgb(${map(screens, 2, 3, 52, 127)}, ${map(screens, 2, 3, 58, 98)}, ${map(screens, 2, 3, 64, 137)})`);
-        break;
-    case 3: //rgb(127, 98, 137) -> rgb(78, 105, 150)
-        nav.css('background-color', `rgb(${map(screens, 3, 4, 127, 78)}, ${map(screens, 3, 4, 98, 105)}, ${map(screens, 3, 4, 137, 150)})`);
-        break;
-
-}
+    switch(numScreens) {
+        case 0: // grey -> green, rgb(52, 58, 64) -> rgb(68, 175, 105)
+            nav.css('background-color', `rgb(${map(screens, 0, 1, 52, 68)}, ${map(screens, 0, 1, 58, 175)}, ${map(screens, 0, 1, 64, 105)}) `);
+            break;
+        case 1: // green -> grey, rgb(68, 175, 105) -> rgb(52, 58, 64)
+            nav.css('background-color', `rgb(${map(screens, 1, 2, 68, 52)}, ${map(screens, 1, 2, 175, 58)}, ${map(screens, 1, 2, 105, 64)})`);
+            break;
+        case 2: // grey -> blue, rgb(52, 58, 64) -> rgb(78, 105, 150)
+            nav.css('background-color', `rgb(${map(screens, 2, 3, 52, 78)}, ${map(screens, 2, 3, 58, 105)}, ${map(screens, 2, 3, 64, 150)})`);
+            break;
+    }
 })
 
-//window.requestAnimationFrame(update);
+function setCards() {
+    setColorsTheme();
+    setListeners();
+}
+
+function setListeners() {
+    for (let i = 1; i <= document.getElementsByClassName('card').length; i++) {
+        document.getElementById(`card${i}`).addEventListener('click', () => {
+            if (selectedProject === '') {
+                selectedProject = `card${i}`;
+                $('#selected-project').show();
+                $('#project-list').hide();
+                // decorate selected project
+                $('#project-title').html($(`#card${i} p`).html());
+                $('#project-description').html(descriptions[i-1].description);
+                $('#selected-project div').css('background-color', $(`#card${i}`).css('background-color'));
+                
+            }
+        })
+    }
+}
+
+function closeProject() {
+    selectedProject = '';
+    $('#selected-project').hide();
+    $('#project-list').show();
+}
+
+function setColors() {
+    const classes = ['card-1', 'card-2', 'card-3', 'card-4', 'card-5'];
+    const cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        let idx = Math.floor(Math.random() * classes.length);
+        let className = classes[idx];
+        cards[i].classList.remove(classes);
+        cards[i].classList.add(className);
+    }
+}
+
+function setColorsTheme() {
+    const colors = ['#15557D', '#0B9BD9', '#06A191', '#10B37C', '#237A63', '#065052', '#01754B', '#076BB3'];
+    const cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        let idx = Math.floor(Math.random() * colors.length);
+        let color = colors[idx];
+        cards[i].style.backgroundColor = color;
+    }
+}
+
+function setColorsRandom() {
+    const blue = {r: 20, g: 4, b: 243};
+    const green = {r: 7, g: 242, b: 128};
+    const cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        const r = Math.random();
+        cards[i].style.backgroundColor = `rgb(${map(r, 0, 1, blue.r, green.r)}, ${map(r, 0, 1, blue.g, green.g)}, ${map(r, 0, 1, blue.b, green.b)})`;
+    }
+}
+
+const map = (x, start1, end1, start2, end2) => (((x - start1) / (end1 - start1) * (end2 - start2)) + start2);
